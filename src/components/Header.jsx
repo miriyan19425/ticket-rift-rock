742eec2186ef648cb386f2309ede6394445b03c1
miriyan19425 <miriyan19425@gmail.com>
2025-06-22 22:27:28 +0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Form from "./Form";
+import BottomContent from "./BottomContent";
+
+function Header(){
+    return (
+        <header class="body">
+            <h1>Ticket Rift Rock</h1>
+            <Form />
+            <BottomContent />
+        </header>
+    );
+}
+
+export default Header;
