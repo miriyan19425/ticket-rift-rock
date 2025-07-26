0fd9163124e19f3508cf88bc6c70ac746545f64e
miriyan19425 <miriyan19425@gmail.com>
2025-07-26 23:11:28 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import Header from "./components/Header";
+import TicketForm from "./components/TicketForm";
 
 function App() {
-   return <Header />;
+   return (
+            <div className="body">
+                <h1>Ticket Rift Rock</h1>
+                <TicketForm />
+                <div className="bottom-content">
+                    <div className="preview bot">
+                        <ul id="ticket-preview"></ul>
+                    </div>
+                    <div className="purchased bot">
+                        <ul id="ticket-purchase">
+                        </ul>
+                    </div>
+                </div>
+            </div>
+   );
 }
 
 export default App;
