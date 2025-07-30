97e461419ed01866e9636b34f06812767af2df29
miriyan19425 <miriyan19425@gmail.com>
2025-07-30 23:07:58 +0300

    Update and rename BottomContent.jsx to TicketPreview.jsx

---
+++ src/components/TicketPreview.jsx
@@ -0,0 +1,9 @@
+function TicketPreview(){
+    return (
+        <div className="preview bot">
+                <ul id="ticket-preview"></ul>
+        </div>
+    );
+}
+
+export default TicketPreview;
