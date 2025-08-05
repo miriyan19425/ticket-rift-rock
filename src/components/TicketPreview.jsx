950336fc2ad4a7a5c19308bfac7fc5f397851bce
miriyan19425 <miriyan19425@gmail.com>
2025-08-05 23:29:36 +0300

    Update TicketPreview.jsx

--- src/components/TicketPreview.jsx
+++ src/components/TicketPreview.jsx
@@ -1,18 +1,17 @@
 function TicketPreview({ticketPurchase}){
+    console.log(ticketPurchase);
     return (
         <div className="preview bot">
                 <ul id="ticket-preview">
                     {(  
-                        ticketPurchase.numTickets !== 0 && 
-                        ticketPurchase.preference !== '' &&
-                        ticketPurchase.fullName !== '' &&
-                        ticketPurchase.email !== '' &&
-                        ticketPurchase.phoneNumber !== ''
-                     ) &&
+                        ticketPurchase.numTickets === '' && 
+                        ticketPurchase.fullName === '' &&
+                        ticketPurchase.email === '' &&
+                        ticketPurchase.phoneNumber === ''
+                     ) ||
                     <li className="ticket-purchase">
                         <article>
                             <p>Count: {ticketPurchase.numTickets}</p>
-                            <p>Preference: {ticketPurchase.preference}</p>
                             <p>To: {ticketPurchase.fullName}</p>
                             <p>Email: {ticketPurchase.email}</p>
                             <p>Phone Number: {ticketPurchase.phoneNumber}</p>
