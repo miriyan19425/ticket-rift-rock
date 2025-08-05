d8e480be67e5072aa16e915408ca2e1abc0b4628
miriyan19425 <miriyan19425@gmail.com>
2025-08-05 23:27:32 +0300

    Update TicketForm.jsx

--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -1,10 +1,7 @@
 import {useState} from 'react';
 
-const seatingOptions = ["Seating Preference", "General Admission", "VIP"];
-
 function TicketForm({buyTickets}){
-    const [numTickets, setNumTickets] = useState(1);
-    const [preference, setPreference] = useState('Seating Preference');
+    const [numTickets, setNumTickets] = useState('');
     const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
@@ -12,14 +9,12 @@ function TicketForm({buyTickets}){
     function handleSubmit(e){
         e.preventDefault();
 
-        if (!numTickets || !preference || !fullName || !email || !phoneNumber) return;
+        if (!numTickets || !fullName || !email || !phoneNumber) return;
 
-        const data = {numTickets, preference, fullName, email, phoneNumber};
-         
+        const data = {numTickets, fullName, email, phoneNumber};
+        
         buyTickets(data);
-
-        setNumTickets(0);
-        setPreference('');
+        setNumTickets('');
         setFullName('');
         setEmail('');
         setPhoneNumber('');
@@ -29,24 +24,12 @@ function TicketForm({buyTickets}){
         <main id="content">
                     <div className="form-container">
                         <form className="ticket-form" onSubmit={handleSubmit} >
-                            <input type="number" placeholder="Number of Tickets" min="1" value={numTickets} onChange={(e) => setNumTickets(e.target.value)} />
-                            <select onChange={(e) => setPreference(e.target.value)} >
-                                {seatingOptions.map((option, index) => (
-                                    <option
-                                        key={index}
-                                        value={option.toLowerCase().replace(/\s/g, '-')}
-                                        disabled={index === 0}
-                                        selected={index === 0}
-                                    >
-                                        {option}
-                                    </option>
-                                ))}
-                            </select>
-                            <input type="text"  placeholder="Full Name"     value={fullName}    onChange={(e) => setFullName(e.target.value)} />
-                            <input type="email" placeholder="Email"         value={email}       onChange={(e) => setEmail(e.target.value)} />
-                            <input type="tel"   placeholder="Phone Number"  value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                        </form>
-                        <button>Purchase Tickets</button>
+                            <input type="number" placeholder="Number of Tickets"    value={numTickets}  onChange={(e) => setNumTickets(e.target.value)} />
+                            <input type="text"   placeholder="Full Name"            value={fullName}    onChange={(e) => setFullName(e.target.value)} />
+                            <input type="email"  placeholder="Email"                value={email}       onChange={(e) => setEmail(e.target.value)} />
+                            <input type="tel"    placeholder="Phone Number"         value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                            <button>Purchase Tickets</button>
+                        </form>   
                     </div>
         </main>
     );
