408f0312e2f635a19e04377de7ad522dbbec214b
miriyan19425 <miriyan19425@gmail.com>
2025-10-11 23:33:52 +0300

    Update TicketPreview.jsx

--- src/components/TicketPreview.jsx
+++ src/components/TicketPreview.jsx
@@ -1,34 +1,31 @@
 function TicketPreview({ticketPurchase}){
-     
-    return (
-        <div className="preview bot">
-                
-                    {(  
-                        ticketPurchase.numTickets === 0 && 
-                        ticketPurchase.preference === 'Seating Preference' &&
-                        ticketPurchase.fullName === '' &&
-                        ticketPurchase.email === '' &&
-                        ticketPurchase.phoneNumber === ''
-                     ) ||
-                         <ul id="ticket-preview">
-                    <li className="ticket-purchase">
-                        <article>
-                            <p>Count: {ticketPurchase.numTickets}</p>
-                            <p>Preference: {ticketPurchase.preference}</p>
-                            <p>To: {ticketPurchase.fullName}</p>
-                            <p>Email: {ticketPurchase.email}</p>
-                            <p>Phone Number: {ticketPurchase.phoneNumber}</p>
-                        </article>
-                        <div className="btn-container">
-                            <button className="edit-btn">Edit</button>
-                            <button className="next-btn">Next</button>
-                        </div>
-                    </li>
+     return (
+          <div className="preview bot">
+               {(  
+                    ticketPurchase.numTickets === 0 && 
+                    ticketPurchase.preference === 'Seating Preference' &&
+                    ticketPurchase.fullName === '' &&
+                    ticketPurchase.email === '' &&
+                    ticketPurchase.phoneNumber === ''
+               ) ||
+                    <ul id="ticket-preview">
+                         <li className="ticket-purchase">
+                              <article>
+                                   <p>Count: {ticketPurchase.numTickets}</p>
+                                   <p>Preference: {ticketPurchase.preference}</p>
+                                   <p>To: {ticketPurchase.fullName}</p>
+                                   <p>Email: {ticketPurchase.email}</p>
+                                   <p>Phone Number: {ticketPurchase.phoneNumber}</p>
+                              </article>
+                              <div className="btn-container">
+                                   <button className="edit-btn">Edit</button>
+                                   <button className="next-btn">Next</button>
+                              </div>
+                         </li>
                     </ul>
-                    }
-                
-        </div>
-    );
+               }
+          </div>
+     );
 }
 
 export default TicketPreview;
